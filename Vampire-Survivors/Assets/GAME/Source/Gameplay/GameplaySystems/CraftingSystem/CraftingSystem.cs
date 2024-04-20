1de4efc875a4b3d79332ce9c122b7d794405154f
Onur Gedik <gazi.onur98@gmail.com>
2024-04-20 12:30:28 +0300

    Skill And Enemy Balances

--- Vampire-Survivors/Assets/GAME/Source/Gameplay/GameplaySystems/CraftingSystem/CraftingSystem.cs
+++ Vampire-Survivors/Assets/GAME/Source/Gameplay/GameplaySystems/CraftingSystem/CraftingSystem.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using VampireSurvivors.Gameplay.Systems.AIControl;
 using VampireSurvivors.Gameplay.Systems.BattleSys;
 using VampireSurvivors.Gameplay.Systems.CraftingSys;
@@ -15,12 +16,12 @@ namespace VampireSurvivors.Gameplay.Systems
 
         private PlayerCrafting _playerCraftig = new PlayerCrafting();
 
-        private VSTimerCounter _enemyCreateTimer = new VSTimerCounter(30,28);
+        private VSTimerCounter _enemyCreateTimer = new VSTimerCounter(30, 28);
 
         public IProperty<IUnitHealth> PlayerUnitHealth => _playerUnitHealth;
         private Property<IUnitHealth> _playerUnitHealth;
-        private EnemyWaveDatas _enemyWaveData;
-        private int _currentWave=0;
+        private List<WaveData> _enemyWaveDatas;
+        private int _currentWave = 0;
 
 
         public CraftingSystem(PlayerControlSystem a_playeControlSystem,
@@ -30,8 +31,8 @@ namespace VampireSurvivors.Gameplay.Systems
                               DamageSourceTypeRecorder a_damageSourceTypeRecorder,
                               EnemyWaveDatas a_enemyWaveData)
         {
-            _enemyWaveData = a_enemyWaveData;
-               _playerUnitHealth = new Property<IUnitHealth>(null);
+            _enemyWaveDatas = a_enemyWaveData.WaveDatas;
+            _playerUnitHealth = new Property<IUnitHealth>(null);
             _playerCraftig.CraftPlayer(a_playeControlSystem, a_playerHPFrame, a_damageableRecorder, _playerUnitHealth);
 
             EnemyUnitFactory enemyFactory = new EnemyUnitFactory(a_playeControlSystem.Position, a_enemyMovementControl, a_damageableRecorder, a_damageSourceTypeRecorder);
@@ -43,22 +44,20 @@ namespace VampireSurvivors.Gameplay.Systems
         public override void Update()
         {
             base.Update();
-            EnemyCreateProcess();
+            if (_enemyWaveDatas.Count > _currentWave)
+            {
+                EnemyCreateProcess();
+            }
         }
 
 
         private void EnemyCreateProcess()
         {
-
             if (_enemyCreateTimer.Process())
             {
-                 _enemyUnitCraftingSystem.CreateEnemy(_enemyWaveData.WaveDatas[_currentWave]);
+                _enemyUnitCraftingSystem.CreateEnemy(_enemyWaveDatas[_currentWave]);
                 _currentWave++;
             }
-
         }
-
-
-
     }
 }
--- Vampire-Survivors/Assets/GAME/Source/Gameplay/GameplaySystems/SkillSystem/Skills/Knife/KnifeBehavior.cs
+++ Vampire-Survivors/Assets/GAME/Source/Gameplay/GameplaySystems/SkillSystem/Skills/Knife/KnifeBehavior.cs
@@ -3,7 +3,7 @@ namespace VampireSurvivors.Gameplay.Systems.SkillSys
 {
     public class KnifeBehavior :SkillBehaviour
 {
-        [SerializeField] private float _speed =10;
+        [SerializeField] private float _speed =30;
         private Vector3 _direction;
         private Vector3 _startPosition;
         public void Settings(Vector3 a_direction,Vector3 a_position)
